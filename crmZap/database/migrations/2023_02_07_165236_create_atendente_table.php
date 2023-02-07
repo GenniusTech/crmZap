8eee33eb6c6ff949c39c35dd6c4ce4a5c1bb33e8
wendel <uendel.1@hotmail.com>
2023-02-07 14:29:09 -0300

    criacao das migrations para gerar as tabelas do  banco de dados

---
+++ crmZap/database/migrations/2023_02_07_165236_create_atendente_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atendente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('email', 100);
+            $table->string('tell', 100);
+            $table->string('senha', 32);
+            $table->integer('dep');
+            $table->integer('tipo');
+            $table->integer('status')->unsigned()->default(0)->comment('0: inactive, 1: active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atendente');
+    }
+};
---
+++ crmZap/database/migrations/2023_02_07_165552_create_leads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome',100);
+            $table->string('email',100);
+            $table->string('tell',100);
+            $table->unsignedInteger('chat');
+            $table->unsignedInteger('id_atend');
+            $table->dateTime('data_cadastro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leads');
+    }
+};
