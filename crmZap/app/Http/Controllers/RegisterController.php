c43be0d59c6f73aa0c4433608fa6ed1e0b17d620
wendel <uendel.1@hotmail.com>
2023-02-13 15:54:45 -0300

    atualização da feature

--- crmZap/app/Http/Controllers/RegisterController.php
+++ crmZap/app/Http/Controllers/RegisterController.php
@@ -91,6 +91,6 @@ class RegisterController extends Controller
     public function logout()
     {
         Auth::logout();
-        return redirect()->route('login');
+        return redirect()->route('login');//
     }
 }
